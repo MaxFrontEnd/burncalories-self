d3c1099ba0deda682e1e27ef9772635d2fd4e0e6
maxFrontEnd <m_samohvalov@mail.ru>
2018-11-25 20:28:27 +0300

    count km and calories

--- src/App.js
+++ src/App.js
@@ -5,33 +5,67 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      total: 0,
-      today: 0
+      burn: 0,
+      km: 0,
+      burned: 0,
+      value: ""
     };
   }
+
+  addCalories = () => {
+    let km = 0;
+    let burn = 0;
+    if (this.state.value == 0 || this.state.value === null) {
+      alert("Нечего сжечь");
+    } else {
+      km = Math.round((+this.state.value * 0.5) / 1000);
+      burn = Math.round(km * 0.5 * 75);
+      this.setState({ km, burn, value: "", burned: burn });
+      this.setState(state => {
+        return {
+          total: +state.burned + +state.burn
+        };
+      });
+    }
+  };
+
+  inputNum = event => {
+    if (event.target.value) {
+      this.setState({ value: event.target.value });
+    }
+  };
   render() {
     return (
       <div>
         <div className="input">
-          <span>
-            <label htmlFor="calories">
-              <div>Введите количество шагов</div>
-            </label>
-          </span>
-          <span className="input-button">
-            <input type="number" name="calories" id="calories" />
-            <button className="buttons">Cжечь</button>
-          </span>
+          <label htmlFor="calories">
+            <div>Введите количество шагов</div>
+            <span className="input-button">
+              <input
+                type="number"
+                name="calories"
+                id="calories"
+                onChange={this.inputNum}
+                value={this.state.value}
+              />
+              <button className="buttons" onClick={this.addCalories}>
+                Cжечь
+              </button>
+            </span>
+          </label>
         </div>
         <div className="results">
           <div>
             <div className="total-today">
-              <p>Вы сожгли {this.state.today} каллорий за сегодня</p>
+              <p>
+                Вы прошли <span className="num">{this.state.km}</span>{" "}
+                километра/ров
+              </p>
             </div>
             <div className="total-text">
               <p>
-                Вы сожгли всего
-                {this.state.total} каллорий{" "}
+                Вы сожгли всего <span className="num">{this.state.total}</span>{" "}
+                каллорий{" "}
               </p>
             </div>
           </div>
